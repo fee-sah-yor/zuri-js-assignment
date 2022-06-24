01a5c2e6668b703847c3b82cb53cfd6faebad4dd
fee-sah-yor <101174144+fee-sah-yor@users.noreply.github.com>
2022-06-24 09:54:28 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,3 @@
+const name = document.write("<p> <b>Name</b>: My name is Afowowe Fisayo </p>");
+const height = document.write("<p> <b>Height</b>: 5'7 </p>");
+const country = document.write("<p> <b>Country</b>: Nigeria </p>");
